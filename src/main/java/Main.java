df5b0a5704255752b564f26b2ba95ba8cd23d62a
dasbene <9532418+dbene@users.noreply.github.com>
2020-01-15 23:12:45 +0100

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
 
 import cg.algorithms.blatt4.D2Tree;
+import cg.algorithms.grahamsscan.GrahamsScan;
+import cg.algorithms.grahamsscan.Point;
 import cg.algorithms.polygonsintersect.IntersectConvexPolygon;
 import cg.algorithms.polygonsintersect.Polygon;
 import cg.algorithms.quadtrees.QuadTree;
@@ -12,46 +17,43 @@ import cg.algorithms.utils.SceneProgress;
 public class Main {
 
 	public static void main(String[] args) {
-		blatt3();
+		Path drawerPath = Paths.get("C:\\Users\\dasbene\\Desktop\\drawer_dev\\cg_drawer");
+
+		blatt1(drawerPath);
+		blatt2(drawerPath);
+		blatt3(drawerPath);
+		blatt4(drawerPath);
 	}
 
-	public static void blatt4() {
+	public static void blatt1(Path drawerPath) {
 		// Read Object
-		File points = new File(ClassLoader.getSystemClassLoader().getResource("blatt4_1.obj").getFile());
-		File range = new File(ClassLoader.getSystemClassLoader().getResource("blatt4_range.obj").getFile());
+		File objFile = new File(ClassLoader.getSystemClassLoader().getResource("test.obj").getFile());
 
-		D2Tree d2t = null;
 		try {
-			String pointsString = FileUtils.readFileToString(points, "UTF-8");
-			String rangeString = FileUtils.readFileToString(range, "UTF-8");
-			d2t = new D2Tree(pointsString, rangeString);
+			String[] objLines = FileUtils.readFileToString(objFile, "UTF-8").split(System.getProperty("line.separator"));
+
+			ArrayList<Point> points = new ArrayList<Point>();
+			for (int i = 0; i < objLines.length; i++) {
+				Point point = new Point(objLines[i]);
+				points.add(point);
+			}
+
+			GrahamsScan gs = new GrahamsScan(points);
+			cg.algorithms.grahamsscan.SceneProgress sp = gs.process();
+
+			try {
+				sp.saveContextToJson(Paths.get(drawerPath.toString(), "algorithms", "blatt2", "data", "output.json").toFile());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 
-		d2t.process();
-
-		d2t.saveToJSON(new File("C:\\Users\\dasbene\\Desktop\\drawer_dev\\ComputationalGeometryDrawer\\data\\test_blatt4.json"));
 	}
 
-	public static void blatt3() {
-		// Read Object
-		File quad = new File(ClassLoader.getSystemClassLoader().getResource("UB2_T3.obj").getFile());
-
-		QuadTree qt = null;
-		try {
-			String quadString = FileUtils.readFileToString(quad, "UTF-8");
-			qt = new QuadTree(quadString);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		qt.process();
-
-		qt.saveToJSON(new File("C:\\Users\\dasbene\\Desktop\\drawer_dev\\ComputationalGeometryDrawer\\data\\blatt3.json"));
-	}
-
-	public static void blatt2() {
+	public static void blatt2(Path drawerPath) {
 		// Read Object
 		File poly1File = new File(ClassLoader.getSystemClassLoader().getResource("poly1.obj").getFile());
 		File poly2File = new File(ClassLoader.getSystemClassLoader().getResource("poly2_v2.obj").getFile());
@@ -71,10 +73,46 @@ public class Main {
 		SceneProgress sp = icp.process();
 
 		try {
-			sp.saveContextToJson(new File("C:\\Users\\dasbene\\Desktop\\drawer_dev\\ComputationalGeometryDrawer\\data\\blatt2.json"));
+			sp.saveContextToJson(Paths.get(drawerPath.toString(), "algorithms", "blatt2", "data", "output.json").toFile());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void blatt3(Path drawerPath) {
+		// Read Object
+		File quad = new File(ClassLoader.getSystemClassLoader().getResource("quad_2.obj").getFile());
+
+		QuadTree qt = null;
+		try {
+			String quadString = FileUtils.readFileToString(quad, "UTF-8");
+			qt = new QuadTree(quadString);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		qt.process();
+
+		qt.saveToJSON(Paths.get(drawerPath.toString(), "algorithms", "blatt3", "data", "blatt3.json").toFile());
+	}
+
+	public static void blatt4(Path drawerPath) {
+		// Read Object
+		File points = new File(ClassLoader.getSystemClassLoader().getResource("blatt4_1.obj").getFile());
+		File range = new File(ClassLoader.getSystemClassLoader().getResource("blatt4_range.obj").getFile());
+
+		D2Tree d2t = null;
+		try {
+			String pointsString = FileUtils.readFileToString(points, "UTF-8");
+			String rangeString = FileUtils.readFileToString(range, "UTF-8");
+			d2t = new D2Tree(pointsString, rangeString);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		d2t.process();
+		
+		d2t.saveToJSON(Paths.get(drawerPath.toString(), "algorithms", "blatt4", "data", "test_blatt4.json").toFile());
 	}
 
 }
